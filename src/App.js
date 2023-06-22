65162cf485113c2b2693ab2ba6f94d3f39dc2e0c
songocut02 <nguyenthanhhung.thcneu@gmail.com>
2023-06-22 09:49:09 +0700

    test route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import axios from 'axios'
 import Formtable from './components/Formtable';
 import Login from './components/Login';
 import SignUp from './components/Signup';
@@ -8,24 +9,25 @@ import UserDetails from './components/userDetails';
 import Product from './components/Product';
 import Cart from './components/Cart';
 
-axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
+axios.defaults.baseURL = 'https://backendfinal-u9mo.onrender.com/admin';
 
 function App() {
-  const isLoggedIn = window.localStorage.getItem("loggedIn");
-
+  const isLoggedIn = window.localStorage.getItem("loggedIn")
   return (
     <Router>
       <Routes>
         <Route
-          exact
-          path="/"
-          element={isLoggedIn === "true" ? <UserDetails /> : <Login />}
-        />
-        <Route path="/admin" element={<Formtable />} />
+            exact
+            path="/"
+            element={isLoggedIn == "true" ? <UserDetails /> : <Login />}
+          />
+        {/* <Route path="https://frontendfinal-5h94.onrender.com/admin" element={<Formtable />} /> */}
+        <Route path="/product" element={<Product />} />
         <Route path="/sign-in" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/userDetails" element={<UserDetails />} />
-        <Route path="/product" element={<Product />} />
+
+        
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
