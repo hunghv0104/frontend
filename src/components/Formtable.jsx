f823bf9abae77b00eaa750943e66dd63d0f6bcb9
songocut02 <nguyenthanhhung.thcneu@gmail.com>
2023-06-24 17:14:53 +0700

    ok

--- src/components/Formtable.jsx
+++ src/components/Formtable.jsx
@@ -75,7 +75,7 @@ const handleSubmit = async(e)=>{
   formDataWithImage.append('category', formData.category);
   formDataWithImage.append('description', formData.description);
   formDataWithImage.append('image', formData.image);
-  console.log(formDataWithImage)
+  
   const data = await axios.post("/create", formDataWithImage);
 
   console.log(data)
